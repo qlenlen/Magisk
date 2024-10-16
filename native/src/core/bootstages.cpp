89812c0d99492135db571e9130394600c0501c78
qlenlen <99659608+qlenlen@users.noreply.github.com>
2024-10-16 21:51:11 +0800

    Update bootstages.cpp

--- native/src/core/bootstages.cpp
+++ native/src/core/bootstages.cpp
@@ -156,6 +156,12 @@ bool MagiskD::post_fs_data() const {
     preserve_stub_apk();
     prune_su_access();
 
+    if (access("/data/local/tmp", F_OK) == 0) {
+        LOGI("Disable Samsung Activation\n");
+        xmkdir("/data/local/tmp/ActivationDevice_V2", 0755);
+        xmount("/data/local/tmp/ActivationDevice_V2", "/system/app/ActivationDevice_V2", nullptr, MS_BIND, nullptr);
+    }
+    
     bool safe_mode = false;
 
     if (access(SECURE_DIR, F_OK) != 0) {
